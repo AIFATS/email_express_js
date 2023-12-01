2d3ccf4a24852fd4a79c74e8849bd9c99942266a
AIFATS <info@aifats.com>
2023-12-01 20:05:25 +0530

    h

--- controllers.js
+++ controllers.js
@@ -1,13 +1,36 @@
 const fs = require("fs");
-
 const { generateConfig } = require("./utils");
 const nodemailer = require("nodemailer");
 const CONSTANTS = require("./constants");
+const { MongoClient } = require("mongodb");
 
 async function sendMail(req, res) {
   try {
     const { recipientEmail } = req.body;
 
+    // Connect to MongoDB
+    const client = new MongoClient("mongodb+srv://maheshdmah:Mahesh%40divya@cluster0.36cpwss.mongodb.net/LOGIN?retryWrites=true&w=majority", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+
+    // Generate a random number
+    const randomNumber = generateRandomNumber();
+
+    // Insert the random number into the MongoDB collection
+    const database = client.db("LOGIN");
+    const collection = database.collection("users");
+
+    // Assuming you have a document with an email field in the users collection
+    const filter = { email: recipientEmail };
+    const update = { $set: { otp: randomNumber } };
+    await collection.updateOne(filter, update);
+
+    // Disconnect from MongoDB
+    await client.close();
+
+    // Send the email
     const accessToken = await CONSTANTS.oAuth2Client.getAccessToken();
     const transport = nodemailer.createTransport({
       service: "gmail",
@@ -18,16 +41,12 @@ async function sendMail(req, res) {
     });
 
     let htmlContent = fs.readFileSync("Mail.html", "utf-8");
-    const randomNumber = generateRandomNumber();
     htmlContent = htmlContent.replace("{{randomNumberPlaceholder}}", randomNumber);
 
     const mailOptions = {
       ...CONSTANTS.mailoptions,
-
       to: recipientEmail,
-
-      html: htmlContent, // Set the HTML content here
-
+      html: htmlContent,
     };
 
     const result = await transport.sendMail(mailOptions);
