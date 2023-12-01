837df34e274f3d46af60fe46ae3fd9ef22932495
AIFATS <info@aifats.com>
2023-12-02 00:13:02 +0530

    2

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors"); // Import the cors middleware
 const routes = require("./routes");
 require("dotenv").config();
 
 const app = express();
+
+app.use(cors()); // Enable CORS for all routes
 
 app.use(express.json());
 
@@ -15,6 +18,3 @@ app.use("/api", routes);
 app.get("/", async (req, res) => {
   res.send("Welcome to Gmail API with NodeJS");
 });
-
-
-module.exports = app;
